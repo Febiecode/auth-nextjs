cfe3facc09dc9c953fe2ca1e6b83d75ca802ac96
febiecode <sathyakumar.webdev@gmail.com>
2024-04-28 01:24:17 +0530

    profile page p changed

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
             <div className='flex flex-col items-center justify-center min-h-screen py-2'>
                 <h1>Profile</h1>
                 <hr />
-                <p>Profile Page</p>
+                <p className='text-4xl'>Profile Page</p>
             </div>
         </>
     )
